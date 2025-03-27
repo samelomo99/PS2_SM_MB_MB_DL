7f219f85cf8ea98dca42659541bae79a3f38e578
Diana Carolina Lopera <carolina1523@gmail.com>
2025-03-27 09:00:06 -0500

    se crea script problemset2

---
+++ script/PS2_script_p2.R
@@ -0,0 +1,26 @@
+
+
+#-------------------------------------------------------------------# ----
+## --------------- Problem Set 2: Predicting Poverty -------------- ##
+## - Santiago Melo - Miguel Blanco - María Bernal - Diana Lopera - ##
+#-------------------------------------------------------------------#
+
+
+# ----------------------LIBRERIAS----------------------------------- # ----
+
+
+
+# ----------------------DATOS--------------------------------------- # ----
+
+# Usamos la base de datos scrapeada y subida al repositorio en GitHub
+
+datos <- read_csv(
+  "https://raw.githubusercontent.com/samelomo99/PS1_SM_MB_MB_DL/main/stores/GEIH_2018_sample_all.csv"
+)
+
+
+# ----------------------DESCRIPTIVAS-------------------------------- # ----
+
+
+
+# ----------------------MODELOS------------------------------------- # ----
