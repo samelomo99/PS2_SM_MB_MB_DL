46710c582436c477ad6c63c3b3a5c2d30fed5372
Diana Carolina Lopera <carolina1523@gmail.com>
2025-03-28 10:35:28 -0500

    se cargan los datos

--- script/PS2_script_p2.R
+++ script/PS2_script_p2.R
@@ -8,20 +8,42 @@
 
 # ----------------------LIBRERIAS----------------------------------- # ----
 
+library(tidyverse)
+library(skimr)  # For quick summaries
+library(naniar) # For missing data
 
 # ----------------------DATOS--------------------------------------- # ----
 
+#Los archivos de datos son demasiado grandes para GitHub. Lee los datos del siguiente enlace:
 
-# Definir rutas
+# URLs de los datasets
+train_hogares_url <- "https://cgiar-my.sharepoint.com/personal/d_c_lopera_cgiar_org/_layouts/15/download.aspx?share=ERoWsIAssWZFlt2ffTOvAkMBptv6eu7iCYmDmiDCKAPeuw"
+train_personas_url <- "https://cgiar-my.sharepoint.com/personal/d_c_lopera_cgiar_org/_layouts/15/download.aspx?share=EdAlNQlv7TNBkaV3ZSFIrLEBS4O1JH6ph7gD2dBUWy4z8w"
+test_hogares_url <- "https://cgiar-my.sharepoint.com/personal/d_c_lopera_cgiar_org/_layouts/15/download.aspx?share=ERh0vDharuJGniJ2ClmQ9TMBYoP7WLeMgWtWTmHWpEnh7A"
+test_personas_url <- "https://cgiar-my.sharepoint.com/personal/d_c_lopera_cgiar_org/_layouts/15/download.aspx?share=Ec1ococzdcVNgHfZziXibXwBCQFDPh2MdoDi_syhIg5LYw"
 
-setwd("D:/OneDrive - CGIAR/Pictures/Diplomado_BigData/Repositorios/PS2_SM_MB_MB_DL")
-repo_path <- "D:/OneDrive - CGIAR/Pictures/Diplomado_BigData/Repositorios/PS2_SM_MB_MB_DL"
-datos_path <- file.path(repo_path, "datos")
+# Descargar archivos temporales
+temp_train_hogares <- tempfile(fileext = ".csv")
+temp_train_personas <- tempfile(fileext = ".csv")
+temp_test_hogares <- tempfile(fileext = ".csv")
+temp_test_personas <- tempfile(fileext = ".csv")
 
-# Crear carpeta "datos" si no existe
-if (!dir.exists(datos_path)) {
-  dir.create(datos_path, recursive = TRUE)
-}
+download.file(train_hogares_url, destfile = temp_train_hogares, mode = "wb")
+download.file(train_personas_url, destfile = temp_train_personas, mode = "wb")
+download.file(test_hogares_url, destfile = temp_test_hogares, mode = "wb")
+download.file(test_personas_url, destfile = temp_test_personas, mode = "wb")
+
+# Cargar los datos en R (asumiendo que son archivos CSV)
+train_hogares <- read.csv(temp_train_hogares)
+train_personas <- read.csv(temp_train_personas)
+test_hogares <- read.csv(temp_test_hogares)
+test_personas <- read.csv(temp_test_personas)
+
+# Limpiar archivos temporales
+unlink(temp_train_hogares)
+unlink(temp_train_personas)
+unlink(temp_test_hogares)
+unlink(temp_test_personas)
 
 
 # ----------------------DESCRIPTIVAS-------------------------------- # ----
