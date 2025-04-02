0726e63ab0c79c286f039dc222a33531344f9e9e
samelomo99 <s.melom2@uniandes.edu.co>
2025-04-02 00:48:54 -0500

    Script

--- script/PS2_script_p2.R
+++ script/PS2_script_p2.R
@@ -1,12 +1,12 @@
 
 
-#-------------------------------------------------------------------# ----
-## --------------- Problem Set 2: Predicting Poverty -------------- ##
+#-------------------------------------------------------------------#
+## --------------- Problem Set 2: Predicting Poverty ------------- ##
 ## - Santiago Melo - Miguel Blanco - María Bernal - Diana Lopera - ##
 #-------------------------------------------------------------------#
 
 
-# ----------------------LIBRERIAS----------------------------------- # ----
+# ---------- LIBRERÍAS ---------- #
 
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("skimr")) install.packages("skimr")
@@ -18,9 +18,9 @@ library(skimr)
 library(ggplot2)
 library(DataExplorer)
 
-# ----------------------DATOS--------------------------------------- # ----
+# ---------- DATOS ---------- #
 
-#Los archivos de datos son demasiado grandes para GitHub. Lee los datos del siguiente enlace:
+#- Los archivos de datos son demasiado grandes para GitHub. Lee los datos del siguiente enlace:
 
 # URLs de los datasets
 train_hogares_url <- "https://cgiar-my.sharepoint.com/personal/d_c_lopera_cgiar_org/_layouts/15/download.aspx?share=ERoWsIAssWZFlt2ffTOvAkMBptv6eu7iCYmDmiDCKAPeuw"
@@ -53,28 +53,58 @@ unlink(temp_test_personas)
 
 
 
-#---------------------Manipulacion de los datos -------------------------------------
+# ---------- MERGE Y CREACIÓN DE VARIABLES ---------- #
 
-#### Uniendo a nivel individuo####################
+# TRAIN DATA
+
+## Uniendo a nivel individuo
 train_completo_personas <- left_join(train_personas, train_hogares, by = "id")
 head(train_completo_personas)
 
-#\a nivel hogar\#
+#- Generamos variables de ocupados, distinguiendo entre desocupados e inactivos
+train_completo_personas <- train_completo_personas %>%
+  mutate(oc_total = case_when(
+    Oc == 1 ~ 1,
+    Des == 1 ~ 2,
+    Ina == 1 ~ 3,
+    TRUE ~ NA_real_
+  ))
+
+skim(train_completo_personas)
+
+#- Cambiamos NA por 0 para variable de oc, des e ina
+train_completo_personas$Oc[is.na(train_completo_personas$Oc)] <- 0
+train_completo_personas$Des[is.na(train_completo_personas$Des)] <- 0
+train_completo_personas$Ina[is.na(train_completo_personas$Ina)] <- 0
+
+#- Para las variables P6100, P6210, P6210s1 y oficio, cambiamos NA por el valor
+#- correspondiente a no sabe / no responde 
+train_completo_personas$P6100[is.na(train_completo_personas$P6100)] <- 9
+train_completo_personas$P6210[is.na(train_completo_personas$P6210)] <- 9
+# train_completo_personas$Oficio[is.na(train_completo_personas$Oficio)] <- mean(train_completo_personas$Oficio)
+#- Para oficio o cambiaría por mean o directamente elimino los NA
 
 
-
-# Crear resumen por familia
-resumen_familias <- train_personas %>%
+## A nivel hogar
+#- Creamos resumen por hogar
+resumen_familias <- train_completo_personas %>%
   group_by(id) %>%
   summarise(
+    # Variables compartidas (colapsadas a nivel hogar)
+    P6020_moda = names(sort(table(P6020), decreasing = TRUE))[1],  # Moda del género
+    P6040_prom = mean(P6040, na.rm = TRUE),  # Promedio de edad del hogar
+    P6050_jefe = sum(P6050 == 1, na.rm = TRUE),  # Cuántos jefes hay (debería ser 1)
+    P6100_moda = names(sort(table(P6100), decreasing = TRUE))[1],
+    P6210_moda = names(sort(table(P6210), decreasing = TRUE))[1],
+    #oficio_moda = names(sort(table(oficio), decreasing = TRUE))[1],
+      
     # Sexo, salud y pensión del jefe del hogar
     sexo_jefe = P6020[P6050 == 1][1],
     salud_jefe = P6090[P6050 == 1][1],
     pension_jefe = P6920[P6050 == 1][1],
-    oc_jefe = Oc[P6050 == 1][1],
-    des_jefe = Des[P6050 == 1][1],
-    ina_jefe = Ina[P6050 == 1][1],
-    
+    edad_jefe = P6040[P6050 == 1][1],
+    oc_jefe = oc_total[P6050 == 1][1],
+
     # Edad promedio del hogar
     edad_promedio = mean(P6040, na.rm = TRUE),
     
@@ -83,6 +113,9 @@ resumen_familias <- train_personas %>%
     
     # Pet: al menos un miembro con Pet == 1
     hogar_pet = as.integer(any(Pet == 1)),
+    
+    # Tasa de dependencia
+    t_dependencia = ifelse(sum(Pet, na.rm = TRUE) > 0, sum(Ina == 1, na.rm = TRUE) / sum(Pet, na.rm = TRUE), NA),
     
     # Ingresos (sumados individualmente)
     ingreso_impa = sum(Impa, na.rm = TRUE),
@@ -102,7 +135,7 @@ resumen_familias <- train_personas %>%
   ) %>%
   ungroup()
 
-# Unir la base de hogares con el resumen de personas a nivel hogar
+#- Unimos la base de hogares con el resumen de personas a nivel hogar
 train_completo_hogares <- left_join(train_hogares, resumen_familias, by = "id")
 
 glimpse(train_completo_hogares)  # Estructura general
