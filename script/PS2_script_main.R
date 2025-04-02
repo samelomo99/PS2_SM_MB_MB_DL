ae842badbf16e7acc74df4340ee3b25815fa12c1
samelomo99 <s.melom2@uniandes.edu.co>
2025-04-02 15:45:38 -0500

    update script

--- script/PS2_script_main.R
+++ script/PS2_script_main.R
@@ -6,25 +6,56 @@
 #-------------------------------------------------------------------#
 
 
-# ---------- LIBRERÍAS ---------- #
+# ---------- LIBRERÍAS ---------- # ----
 
-if (!require("dplyr")) install.packages("dplyr")
-if (!require("skimr")) install.packages("skimr")
-if (!require("ggplot2")) install.packages("ggplot2")
-if (!require("DataExplorer")) install.packages("DataExplorer")
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
 
-library(dplyr)
-library(skimr)
-library(ggplot2)
-library(DataExplorer)
+pacman::p_load(
+  readr,        # Importar datos (ya incluido en tidyverse)
+  labelled,     # Manejo de etiquetas
+  naniar,       # Visualizar datos faltantes
+  DataExplorer, # Gráficos de missing values
+  psych,        # Estadísticas descriptivas
+  rvest,        # Web scraping
+  rio,          # Importar/exportar datos
+  tidyverse,    # Conjunto de paquetes para tidy data (incluye dplyr, ggplot2, etc.)
+  skimr,        # Resumen de datos
+  visdat,       # Visualizar datos faltantes
+  corrplot,     # Gráficos de correlación
+  gridExtra,    # Organización de gráficos
+  MASS,         # Funciones estadísticas diversas
+  stargazer,    # Tablas para salida a TEX
+  chromote,     # Automatización de navegador (útil para scraping avanzado)
+  ggplot2,      # Gráficos (ya incluido en tidyverse)
+  boot,         # Funciones de bootstrap
+  patchwork,    # Combinación de gráficos
+  caret,         # For predictive model assessment
+  purrr,
+  kableExtra,   # Opciones adicionales para kable()
+  dplyr,          # Manipulación de datos
+  summarytools,
+  knitr,          # kable() para generar tablas en LaTeX
+  xtable,
+  tidyr,
+  gmodels
+)
 
+# ---------- BASE DE DATOS ---------- # ----
 
+train <- read_csv(
+  "https://raw.githubusercontent.com/samelomo99/PS2_SM_MB_MB_DL/refs/heads/main/stores/train_completo_hogares.csv"
+  )
+test <- read_csv(
+  "https://raw.githubusercontent.com/samelomo99/PS2_SM_MB_MB_DL/refs/heads/main/stores/test_completo_hogares.csv"
+  )
 
-# ----------------------DESCRIPTIVAS-------------------------------- # ----
+# ---------- DESCRIPCIÓN DATOS ---------- # ----
 
-# ----------------INSPECCIÓN DE LOS DATOS------------------ # ----
+#- Inspección de los datos creados en las nuevas bases de datos, no de los datos
+#- que teníamos desde el principio, es mucho.
 
-# Crear una lista con los datasets para facilitar el análisis
+#- Crear una lista con los datasets para facilitar el análisis
 datasets <- list(
   "train_hogares" = train_hogares,
   "train_personas" = train_personas,
@@ -84,11 +115,17 @@ for(nombre in names(datasets)) {
 }
 
 
-
-# ----------------MANEJO DE DATOS Y CREACIÓN DE VARIABLES RELEVANTAS------------------ # ----
-
+# ---------- MODELOS ---------- # ----
 
 
 
 
-# ----------------------MODELOS------------------------------------- # ----
+
+
+
+
+
+
+
+
+
